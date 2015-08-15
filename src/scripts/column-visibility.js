59cf8e1b60d95cdce98d80fa9189b2fd237a3415
Christian Behon <christian.behon@gmail.com>
2015-08-15 14:37:01 +0200

    Add drop down rows and fix some issues
    * Iterate over columns and rows

--- src/scripts/column-visibility.js
+++ src/scripts/column-visibility.js
@@ -44,8 +44,6 @@ var ngTablePlugins = angular.module('ngTablePlugins', []);
               console.log("HELLO");
               columnVisibilityStorage.saveTest();
             }
-
-
         };
 })();
 
@@ -53,27 +51,75 @@ var ngTablePlugins = angular.module('ngTablePlugins', []);
     "use strict";
 
     angular.module('ngTablePlugins')
-        .directive('colVisibility', columnVisibility);
+        .directive('colVis', columnVisibility);
 
         columnVisibility.$inject = ['$parse'];
 
         function columnVisibility($parse) {
             return {
                 link: link,
-                restrict: 'EA',
+                restrict: 'A',
                 scope: true,
                 controller: 'columnVisibilityCtrl'
             };
 
             function link(scope, element, attrs) {
-                scope.sayHello();
+
+              console.log("COLUMNS: "+attrs.colVisRows);
+              console.log("STORAGE: "+attrs.colVisStorage);
+              var cv = {
+                generate: function generate(event) {
+                  entries(event, element);
+                }
+              };
+              $parse(attrs.colVis).assign(scope.$parent, cv);
             }
 
-            function generateDropDownEntries(rows) {
+            function entries(event, element, dropDown) {
+              var target = angular.element(event.target);
+              removeEntries(target);
+
+              var dropDown = angular.element('<ul class="dropdown-menu">');
+              var rows = element.find('th.header');
+
               angular.forEach(rows, function (row, key) {
+                var rowEntry = angular.element('<li><a>'+ row.textContent.trim() + '</a></li>');
+                rowEntry.bind('click',onRowClick(element, key));
+                dropDown.append(angular.element(rowEntry));
 
               });
+              dropDown.insertAfter(target);
+              console.log(rows);
             }
+
+            function removeEntries(target) {
+              var ul = target.find('ul');
+              ul.remove();
+            }
+
+            function onRowClick(element, key) {
+              console.log("CLICK");
+              var rows = element.find('tr');
+                                                 angular.forEach(rows, function (row, i) {
+
+                                                     var tr = angular.element(row);
+                                                     var tds = tr.find('th');
+                                                     if (tds.length === 0) {
+                                                         tds = tr.find('td');
+                                                     }
+                                                     angular.forEach(tds, function (td, j) {
+                                                         var td = angular.element(td);
+
+                                                         if (key == j) {
+                                                             hideShowColumn(td);
+                                                         }
+                                                     });
+
+
+                                                 });
+
+            }
+
 
             function hideShowColumn(td) {
                 if (td.css("display") == "none") {
