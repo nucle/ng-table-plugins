fb28c60f642e3e6ac7fcbdcec4d3359b96fb9375
Christian Behon <christian.behon@gmail.com>
2015-08-20 02:07:07 +0200

    Add highlight color

--- src/scripts/column-visibility.js
+++ src/scripts/column-visibility.js
@@ -1,11 +1,11 @@
- /**
-  * ngTablePlugins: ngTable + Angular JS
-  *
-  * @created 08.08.15
-  * @author Christian Behon <christian.behon@knusperleicht.at>
-  * @url https://github.com/nucle/ng-table-plugins
-  * @license Apache License Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>
-  */
+/**
+ * ngTablePlugins: ngTable + Angular JS
+ *
+ * @created 08.08.15
+ * @author Christian Behon <christian.behon@knusperleicht.at>
+ * @url https://github.com/nucle/ng-table-plugins
+ * @license Apache License Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>
+ */
 var ngTablePlugins = angular.module('ngTablePlugins', []);
 
 (function () {
@@ -14,19 +14,19 @@ var ngTablePlugins = angular.module('ngTablePlugins', []);
     angular.module('ngTablePlugins')
         .factory('columnVisibilityStorage', columnVisibilityStorage);
 
-        columnVisibilityStorage.$inject = [];
+    columnVisibilityStorage.$inject = [];
 
-        function columnVisibilityStorage() {
-            return {
-              saveTest: saveTest
-            };
-
-            function saveTest() {
-              console.log("saveTest");
-            }
-
-
+    function columnVisibilityStorage() {
+        return {
+            saveTest: saveTest
         };
+
+        function saveTest() {
+            console.log("saveTest");
+        }
+
+
+    };
 })();
 
 (function () {
@@ -35,16 +35,33 @@ var ngTablePlugins = angular.module('ngTablePlugins', []);
     angular.module('ngTablePlugins')
         .controller('columnVisibilityCtrl', columnVisibilityCtrl);
 
-        columnVisibilityCtrl.$inject = ['$scope','columnVisibilityStorage'];
+    columnVisibilityCtrl.$inject = ['$scope', 'columnVisibilityStorage','$cacheFactory','$log'];
 
-        function columnVisibilityCtrl($scope,columnVisibilityStorage) {
-            $scope.sayHello = sayHello;
+    function columnVisibilityCtrl($scope, columnVisibilityStorage,$cacheFactory,$log) {
+        $scope.element = null;
+        $scope.hideColumn = hideColumn;
+        $scope.showColumn = showColumn;
+        $scope.isColumnVisible = isColumnVisible;
+        $scope.cache = $cacheFactory('hiddenColumns');
 
-            function sayHello() {
-              console.log("HELLO");
-              columnVisibilityStorage.saveTest();
-            }
-        };
+
+        function hideColumn(key) {
+            $log.log("Hide column: " + key);
+            $scope.cache.put(key, true);
+            $log.log("HiddenColumns: " + $scope.cache.info());
+        }
+
+        function showColumn(key) {
+            $log.log("Show column: " + key);
+            $scope.cache.remove(key);
+            $log.log("HiddenColumns: " + $scope.cache.info());
+        }
+
+        function isColumnVisible(key) {
+            return $scope.cache.get(key) !== undefined;
+        }
+
+    }
 })();
 
 (function () {
@@ -53,80 +70,117 @@ var ngTablePlugins = angular.module('ngTablePlugins', []);
     angular.module('ngTablePlugins')
         .directive('colVis', columnVisibility);
 
-        columnVisibility.$inject = ['$parse'];
+    columnVisibility.$inject = ['$parse', '$document', '$timeout'];
 
-        function columnVisibility($parse) {
-            return {
-                link: link,
-                restrict: 'A',
-                scope: true,
-                controller: 'columnVisibilityCtrl'
-            };
-
-            function link(scope, element, attrs) {
-
-              console.log("COLUMNS: "+attrs.colVisRows);
-              console.log("STORAGE: "+attrs.colVisStorage);
-              var cv = {
-                generate: function generate(event) {
-                  entries(event, element);
-                }
-              };
-              $parse(attrs.colVis).assign(scope.$parent, cv);
-            }
-
-            function entries(event, element, dropDown) {
-              var target = angular.element(event.target);
-              removeEntries(target);
-
-              var dropDown = angular.element('<ul class="dropdown-menu">');
-              var rows = element.find('th.header');
-
-              angular.forEach(rows, function (row, key) {
-                var rowEntry = angular.element('<li><a>'+ row.textContent.trim() + '</a></li>');
-                rowEntry.bind('click',onRowClick(element, key));
-                dropDown.append(angular.element(rowEntry));
-
-              });
-              dropDown.insertAfter(target);
-              console.log(rows);
-            }
-
-            function removeEntries(target) {
-              var ul = target.find('ul');
-              ul.remove();
-            }
-
-            function onRowClick(element, key) {
-              console.log("CLICK");
-              var rows = element.find('tr');
-                                                 angular.forEach(rows, function (row, i) {
-
-                                                     var tr = angular.element(row);
-                                                     var tds = tr.find('th');
-                                                     if (tds.length === 0) {
-                                                         tds = tr.find('td');
-                                                     }
-                                                     angular.forEach(tds, function (td, j) {
-                                                         var td = angular.element(td);
-
-                                                         if (key == j) {
-                                                             hideShowColumn(td);
-                                                         }
-                                                     });
-
-
-                                                 });
-
-            }
-
-
-            function hideShowColumn(td) {
-                if (td.css("display") == "none") {
-                    td.show();
-                } else {
-                    td.hide();
-                }
-            }
+    function columnVisibility($parse, $document, $timeout) {
+        return {
+            link: link,
+            restrict: 'A',
+            scope: true,
+            controller: 'columnVisibilityCtrl'
         };
-})();
+
+        function link(scope, element, attrs) {
+
+            var cv = {
+
+                generate: function generate(event) {
+                    entries(event, element, attrs, scope);
+                }
+            };
+            // TODO this is only a work around --> better solution chris
+            angular.element($document).ready($timeout(function () {
+
+                    var visibleCols = scope.$eval(attrs.colVisRows);
+                    if (angular.isArray(visibleCols)) {
+                        angular.forEach(visibleCols, function (colId, k) {
+                            console.log("WAHH" + colId);
+                            scope.hideColumn(colId);
+                            var rows = element.find('tr');
+                            angular.forEach(rows, function (row, i) {
+                                var tr = angular.element(row);
+
+                                var tds = tr.find('th');
+                                if (tds.length === 0) {
+                                    tds = tr.find('td');
+                                }
+
+                                angular.forEach(tds, function (td, j) {
+                                    var element = angular.element(td);
+                                    if (colId == j) {
+                                        element.hide();
+                                    }
+                                });
+                            });
+                        });
+                    }
+                }, 250
+            ));
+            console.log("COLUMNS: " + attrs.colVisRows);
+            console.log("STORAGE: " + attrs.colVisStorage);
+
+
+            $parse(attrs.colVis).assign(scope.$parent, cv);
+        }
+
+
+        function entries(event, element, attrs, scope) {
+            var target = angular.element(event.target);
+
+            removeEntries(angular.element(event.currentTarget));
+            var dropDown = angular.element('<ul class="dropdown-menu">');
+
+            var rows = element.find('th.header');
+
+            angular.forEach(rows, function (row, key) {
+                    var rowEntry = angular.element('<li ng-style="myStyle" style="opacity:1.0"><a> ' + row.textContent.trim() + '</a></li>');
+                    var isColumnVisible = scope.isColumnVisible(key);
+
+                    if (isColumnVisible) {
+                        rowEntry.css("opacity",0.4);
+                    }
+                    rowEntry.bind('click', function () {
+                        if(!scope.isColumnVisible(key)) {
+                            scope.hideColumn(key);
+                        } else {
+                            scope.showColumn(key);
+                            rowEntry.css("opacity",1.0);
+                        }
+                        var rows = element.find('tr');
+
+                        angular.forEach(rows, function (row, i) {
+                            var tr = angular.element(row);
+
+                            var tds = tr.find('th');
+                            if (tds.length === 0) {
+                                tds = tr.find('td');
+                            }
+
+                            angular.forEach(tds, function (td, j) {
+                                var element = angular.element(td);
+                                if (key == j) {
+                                    if (element.css("display") == "none") {
+                                        element.show();
+                                    } else {
+                                        element.hide();
+                                    }
+                                }
+                            });
+                        });
+                    });
+                    dropDown.append(angular.element(rowEntry));
+                }
+            )
+            ;
+            dropDown.insertAfter(target);
+            console.log(rows);
+        }
+
+
+        function removeEntries(target) {
+            var ul = target.find('ul');
+            ul.remove();
+        }
+    }
+})
+    ();
